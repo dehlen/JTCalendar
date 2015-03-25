bf5541c7184a30ad4c49dffcfd8a1c594ae50100
David Ehlen <dehlen@me.com>
2015-03-26 00:34:06 +0100

    Fixed a bug where the calendar views wouldn't disappear after dismissing a viewcontroller in a navigation controller. See: https://github.com/jonathantribouharet/JTCalendar/issues/31

--- JTCalendar/JTCalendarContentView.m
+++ JTCalendar/JTCalendarContentView.m
@@ -53,6 +53,8 @@
     self.showsHorizontalScrollIndicator = NO;
     self.showsVerticalScrollIndicator = NO;
     self.pagingEnabled = YES;
+    self.clipsToBounds = YES;
+	
     
     for(int i = 0; i < NUMBER_PAGES_LOADED; ++i){
         JTCalendarMonthView *monthView = [JTCalendarMonthView new];
